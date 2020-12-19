e708156701e70ee25c67109823d769fe12349449
papa <papa@PC-Papa>
2020-12-19 19:50:45 +0100

    prova di commit del file Formula di Erone.

---
+++ Formula_Erone.py
@@ -0,0 +1,16 @@
+﻿def IsItATriangle(a,b,c):
+    return a+b>c and b+c>a and c+a>b
+
+def Heron(a,b,c):
+    p=(a+b+c)/2
+    return(p*(p-a)*(p-b)*(p-c))**0.5
+
+def FieldOfTriangle(a,b,c):
+    if not IsItATriangle(a,b,c,):
+        return None
+    return Heron(a,b,c)
+
+print(FieldOfTriangle(1.,1.,2.**.5))
+    
+"""We try it with a right-angle triangle as a half of a square with one side
+equal to 1. This means that its field should be equal to 0.5."""
