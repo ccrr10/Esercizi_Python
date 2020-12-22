9cb97dff5cacb4c21003eaba4f89055a11d489e8
Rosario TV <canros@ccrr.it>
2020-12-22 10:14:52 +0100

    the first two exercises

---
+++ plural.py
@@ -0,0 +1,45 @@
+import re
+'''def plural(noun):
+
+    if re.search('[sxz]$', noun):
+        return re.sub('$', 'es', noun)
+    elif re.search('[^aeioudgkprt]h$', noun):
+        return re.sub('$', 'es', noun)
+    elif re.search('[^aeiou]y$', noun):
+        return re.sub('y$', 'ies', noun)
+    else:
+        return noun + 's'
+
+print(plural('boy'))'''
+
+def build_match_and_apply_functions(pattern, search, replace):
+    def matches_rule(word):
+        return re.search(pattern, word)
+    def apply_rule(word):
+        return re.sub(search, replace, word)
+    return (matches_rule, apply_rule)
+
+def rules(rules_filename):
+    with open(rules_filename, encoding='utf-8') as pattern_file:
+        for line in pattern_file:
+            pattern, search, replace = line.split(None, 3)
+            yield build_match_and_apply_functions(pattern, search, replace)
+
+def plural(noun, rules_filename='plural-ita-rules.txt'):
+    for matches_rule, apply_rule in rules(rules_filename):
+        if matches_rule(noun):
+            return apply_rule(noun)
+    raise ValueError('no matching rule for {0}'.format(noun))
+
+print(plural('mamma'))
+
+# comma, dramma --> i
+# mamma, salma --> e
+# formicolio --> ii
+# mezzanotte --> mezzenotti
+# mezzamanica --> mezzemaniche 
+
+
+
+
+
